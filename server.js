f8a1ccaf787e4b81d6759b08512a7bba1205409f
Jai Sharma <sharmajai901@gmail.com>
2023-01-17 08:53:45 +0530

    second commit

--- server.js
+++ server.js
@@ -1,8 +1,44 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const app = require('./app');
 
 dotenv.config({ path: './config.env' });
 
-const app = require('./app');
+const DB = process.env.DB_STRING.replace('<PASSWORD>', process.env.DB_PASSWORD);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('connection was successful'));
+
+const toursSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'name is required'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'price is required'],
+  },
+});
+
+const Tour = new mongoose.model('Tour', toursSchema);
+
+// const testTour = new Tour({
+//   name: 'jai',
+//   rating: 5,
+//   price: 200,
+// });
+
+// testTour.save().then((res) => console.log(res));
 
 const port = process.env.PORT || '8000';
 
